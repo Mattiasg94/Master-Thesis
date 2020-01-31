898dea08434d4e83d15b8653feffdb3f044331b5
Mattias Gerle <mattias.gerle@astecon.se>
2020-01-31 13:11:30 +0100

    changes when simons code wont work

--- Mattias/test.m
+++ Mattias/test.m
@@ -1,11 +1,4 @@
 clc
-N = 3;
-% e = ones(N,1);
-% A = spdiags([ones(N,1) ones(N,1)],0:1,N,N+1);
-% A
-% full(A)
-A=zeros(N,N*2)
-for i=1:N
-    A(i,2*i-1:2*i)=ones(1,2)
-end
 
+rectangle('Position',[1 2 5 6])
+axis([0 10 0 10])
