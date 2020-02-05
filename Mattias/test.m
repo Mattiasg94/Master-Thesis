812089f3fb159c54c4a656cf5fc1fcd18b048a4a
Mattias Gerle <mattias.gerle@astecon.se>
2020-02-05 12:51:45 +0100

    Model_DD working

--- Mattias/test.m
+++ Mattias/test.m
@@ -1,21 +1,2 @@
 clc
 clear all
-
-N=2;
-
-
-
-%     Mbelt=zeros(N,N*2);
-%     for i=1:N
-%         Mbelt(i,2*i-1:2*i)=ones(1,2);
-%     end    
-%     if  true %1<=xk(2) && xk(2)<=6 
-%        activate=1;
-%     else
-%         activate=0;
-%     end
-%     Mx=[];
-%     mx=[activate,0];
-%     for i=1:N
-%         Mx=blkdiag(Mx,mx);
-%     end  
