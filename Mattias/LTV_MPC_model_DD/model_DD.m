5ebff36df4d687f0e972964f5c02a6cfe0d93b0f
Mattias Gerle <mattias.gerle@astecon.se>
2020-02-06 12:58:31 +0100

    weird working LTV MPC

--- Mattias/LTV_MPC_model_DD/model_DD.m
+++ Mattias/LTV_MPC_model_DD/model_DD.m
@@ -1,17 +1,17 @@
 clc
 clear all, close all
 
-N=10;
-Nsim=40;
-dt=0.1;
+N=30;
+Nsim=20;
+dt=0.5;
 ur=[0.1;0]; 
 xr=[10;5;0];
-x0=[0;4;0];
-u0=[0;0];
-lb_x=[0 0 -2*pi];
+x0=[0;4;pi/2];
+u0=[1;0];
+lb_x=[-1 -1 -2*pi];
 ub_x=[10 10 2*pi];
-lb_u=[0 -1];
-ub_u=[10 1];
+lb_u=[-1 -0.5];
+ub_u=[1 0.5];
 lb=[-inf -inf -inf lb_x -inf -inf lb_u];
 ub=[ inf  inf  inf ub_x  inf  inf ub_u];
 [u,x,u_tilde,x_tilde,lb,ub,Z0,MQ,MR,Mxr,Mur,Mu_delta]=setup(x0,xr,ur,ub,lb,Nsim,N);
@@ -27,31 +27,38 @@ for i=1:length(obstacles)/2
     scatter(obstacles(2*i-1),obstacles(2*i),'k','LineWidth',1.5)
 end
 grid on,set(gca,'ytick',min(xcont):max(xcont)),set(gca,'xtick',min(ycont):max(ycont))
-% xline(3.5,'--r');
-% xline(4.5,'--r');
-% xline(5.5,'--r');
+
 
 uk=u0;
-for k = 2:Nsim+1 
-    u_tilde(k-1,:)=(uk-ur);
-    x_tilde(k-1,:)=(x(k-1,:)'-xr);
-    [A,B] = Linearized_discrete_DD_model(xr,ur,dt); 
-    [Z,fval,exitflag] = optimizer_fmincon(xr,Z0,A, B,MQ,MR,Mxr,Mur,N,x_tilde(k-1,:)',u_tilde(k-1,:)',x(k-1,:)',lb,ub,obstacles);
-%     obstacles(2)=obstacles(2)+0.05;
-%     obstacles(4)=obstacles(4)-0.05;
+for k = 1:Nsim+1 
+    u_tilde(k,:)=(uk-ur);
+    x_tilde(k,:)=(x(k,:)'-xr);
+    [A,B] = Linearized_discrete_DD_model(x(k,:)',uk,dt);
+    [Z,fval,exitflag] = optimizer_fmincon(x(k,:)',uk,xr,Z0,A,B,MQ,MR,Mxr,Mur...
+        ,N,x_tilde(k,:)',u_tilde(k,:)',lb,ub,obstacles);
     for i=1:length(obstacles)/2
     scatter(obstacles(2*i-1),obstacles(2*i),'k','LineWidth',1.5)
     end
     Z0=Z;
-    u(k-1,:)=Z(N*3*2+1:N*3*2+2)'+ur;
-    uk=u(k-1,:)';    
-    [A,B] = Linearized_discrete_DD_model(x(k-1,:)',uk,dt);
-    x(k,:) =A*x(k-1,:)' + B*u(k-1,:)';
+% This plots the optimizers x and y over the whole horizon. It cannot turn!
+    X_TILDE=Z(1:3*N);
+    X=Z(3*N+1:6*N);
+    for j=1:N
+        X_plot(j,:)=X(3*j-2:3*j);
+        X_TILDE_plot(j,:)=X_TILDE(3*j-2:3*j);
+        plot(X_plot(:,1),X_plot(:,2),'ok')
+        pause(0.1)
+    end
+   figure(2)
+   plot(X_TILDE_plot(:,2))
+    u(k,:)=Z(N*3*2+1:N*3*2+2)'+ur;
+    uk=u(k,:)';    
+    x(k+1,:) =A*x(k,:)' + B*u(k,:)';
      
     if exitflag==-2
        disp("----Unfeasible!----")
        break
-    end
+    end    
     plot(x(:,1),x(:,2),'ok')
     pause(0.01)
     for i=1:N-1
