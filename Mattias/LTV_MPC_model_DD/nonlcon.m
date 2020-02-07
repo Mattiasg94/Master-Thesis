f638fb870172403684003a6925ac6a55a54d31e5
Mattias Gerle <mattias.gerle@astecon.se>
2020-02-07 08:59:55 +0100

    fully working model

--- Mattias/LTV_MPC_model_DD/nonlcon.m
+++ Mattias/LTV_MPC_model_DD/nonlcon.m
@@ -5,9 +5,8 @@ for i=1:N
     [A,B] = Linearized_discrete_DD_model(xk,uk,dt);    
     xk1=Z(N*3+3*i-2:N*3+3*i)';
     ceq(3*i-2:3*i,1)=xk1-A*xk-B*uk;
-    xk=Z(N*3+3*i-2:N*3+3*i)';
-    uk=Z(N*6+2*i-1:N*6+2*i)';
-
+    xk=xk1;
+    uk=Z(N*8+2*i-1:N*8+2*i)';
 end
 
 
